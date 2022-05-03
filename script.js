989289cf13ac6e25326af3443f052f8dc1acb634
TanK109 <tankieu7c@gmail.com>
2022-05-03 17:17:08 +0700

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('hh!')
+console.log('jjjhh!')
+var h1Element = document.querySelector('h1')
+var inputElement = document.querySelector('input[type = "text"]')
+inputElement.oninput = function(e) {
+  console.log(e.target.value) ; 
+}

60dcc9a0e19bc73b2fcc19f74b1eb38080023593
TanK109 <tankieu7c@gmail.com>
2022-05-12 17:59:23 +0700

    1 file modified

--- script.js
+++ script.js
@@ -63,4 +63,8 @@ getComment()
       resolve(result)
     })
   }
+fetch('https://123jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
+  // .catch(err => alert('lỗi1'))
 
